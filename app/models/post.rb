b36019d00a8187e656a9c309a15958833d65ae31
+ <+>
2023-02-24 11:28:08 -0500

    commit tree 5c19bb14d2200a6dc3bd6b7c2821ca6fa97a2303

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    
+    # validate :title_is_clickbait
+    # def title_is_clickbait
+    #     unless title.include?("Won't Believe")
+    #         errors.add(:title, "title not allowed")
+    #     end
+    # end
+    
+    
 end
